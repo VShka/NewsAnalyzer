509dc19431e7fdaad5faa1014fe9edf0637fb2ae
VShka <viktor.shkarupa@yandex.ru>
2020-08-16 07:38:11 +0300

    added methods for working with localStorage

--- src/js/modules/DataStorage.js
+++ src/js/modules/DataStorage.js
@@ -1,7 +1,16 @@
 'use strict'
 
 export default class DataStorage {
-  constructor() {
+  constructor(apiData) {
+    this.apiData = apiData;
+  }
 
+  packData(keyWord) {
+    localStorage.clear(); // очищаем хранилище перед наполнением
+    localStorage.setItem(keyWord, this.apiData);
+  }
+
+  unpackData(keyWord) {
+    JSON.parse(localStorage.getItem(keyWord));
   }
 }
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -5,16 +5,20 @@ import "./style.css"
 import NEWS_API_PROPS from "../../js/constants/news-api-props";
 import ERRORS from "../../js/constants/errors";
 import NewsApi from "../../js/modules/NewsApi";
+import DataStorage from "../../js/modules/DataStorage";
 import SearchInput from "../../js/components/SearchInput";
 import Validation from "../../js/components/Validation";
 
 // переменные
 const form = document.forms.search;
 const formInput = document.querySelector('.form__input');
+const resultBlock = document.querySelector('.result');
+const preloader = resultBlock.querySelector('.preloader');
 
 
 // инстансы классов
 const newsApi = new NewsApi(NEWS_API_PROPS);
+
 const searchInput = new SearchInput(
 
   );
@@ -33,12 +37,13 @@ form.addEventListener('submit', (event) => {
   // отправляем запрос к Api, передаем в метод аргумент (ключевое слово введеное в инпут)
   newsApi.getNews(keyWord)
   .then(data => {
+    resultBlock.classList.remove('result_hidden'); // показали блок с результатом
+    preloader.classList.remove('preloader_hidden'); // показали прелоудер
     // преобразовываем полученные данные в строку
     const newsData = JSON.stringify(data.articles);
-    // очищаем localStorage
-    localStorage.clear();
+    const dataStorage = new DataStorage(newsData);
     // закидываем преобразованные данные в хранилище
-    localStorage.setItem('news', newsData);
+    dataStorage.packData(keyWord);
   })
   .catch(err => console.error('Ошибка с данными:', err.message));
 })
