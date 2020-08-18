6c322f6cc60aa49ac1b2a9bb57c8a9b945b55500
VShka <viktor.shkarupa@yandex.ru>
2020-08-18 17:02:39 +0300

    delete flickity slider

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -9,13 +9,6 @@ import GITHUB_API_PROPS from "../../js/constants/github-api-props";
 import GithubApi from "../../js/modules/GithubApi";
 import CommitCardList from "../../js/components/CommitCardList";
 
-const sliderContainer = document.querySelector('.carousel');
-const flkty = new Flickity(sliderContainer, {
-  freeScroll: true,
-  wrapAround:  true,
-  initialIndex: 0,
-  groupCells: true
-})
 const commitContainer = document.querySelector('.flickity-slider');
 
 const githubApi = new GithubApi(GITHUB_API_PROPS);
