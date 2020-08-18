b3c4e4a13924e41d4131f7104a07e27cefaf1ab2
VShka <viktor.shkarupa@yandex.ru>
2020-08-18 15:19:55 +0300

    1. Add new constant for GithubApi. 2. Add new classes and methods for working with api github and showing commits. 3. Removed caps from layout. 4. Instruction are written in the init about.js file

--- src/js/modules/GithubApi.js
+++ src/js/modules/GithubApi.js
@@ -2,10 +2,25 @@
 
 export default class GithubApi {
   constructor(props) {
-    this.props = props;
+    this.url = props.url;
+    this.owner = props.owner;
+    this.repo = props.repo;
   }
 
   getCommits() {
+    return fetch(`${this.url}` + 'repos/' + `${this.owner}` + `/${this.repo}/` + 'commits')
+    .then(this._checkRequest)
+    .catch(this._catchErr);
+  }
 
+  // проверка состояния запроса
+  _checkRequest(res) {
+    if (res.ok) return res.json();
+
+    return Promise.reject(new Error(res.status));
+  }
+  // отлов ошибки
+  _catchErr(err) {
+    return Promise.reject(new Error(err.message));
   }
 }
--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -2,4 +2,20 @@
 
 import "./style.css";
 
+// constants
+import GITHUB_API_PROPS from "../../js/constants/github-api-props";
 
+// class
+import GithubApi from "../../js/modules/GithubApi";
+import CommitCardList from "../../js/components/CommitCardList";
+
+const commitContainer = document.querySelector('.carousel');
+
+const githubApi = new GithubApi(GITHUB_API_PROPS);
+const commitCardList = new CommitCardList(
+  commitContainer,
+  githubApi
+);
+
+
+commitCardList.renderCommit();
