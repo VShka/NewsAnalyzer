b4d2c1a802768bda257d7e42315d87fbb4cd9ea9
VShka <viktor.shkarupa@yandex.ru>
2020-08-21 04:16:11 +0300

    1) Add method for matches keyword in titles

--- src/js/components/KeywordStatistics.js
+++ src/js/components/KeywordStatistics.js
@@ -22,8 +22,23 @@ export default class KeywordStatistics {
     this.analyticsTitleResult.appendChild(ResultValue);
 
     // упоминаний в заголовках
+    this.titleMention = this._countingMentions();
     const MentionValue = ResultValue.cloneNode(true);
-    MentionValue.textContent = `${this.totalResults}`;
+    MentionValue.textContent = `${this.titleMention.length}`;
     this.analyticsTitleMention.appendChild(MentionValue);
+  }
+
+  // метод для подсчёта упоминаний ключевого слова в заголовках
+  _countingMentions() {
+    const arrayTitles = [];
+    this.dataStat.articles.find(item => {
+      arrayTitles.push(item.title);
+    })
+
+    const arrayMatches = arrayTitles.join(', ').toLowerCase().split(', ')
+    .filter( item => {
+      return item.includes(this.userAsk.toLowerCase());
+    })
+    return arrayMatches;
   }
 }
